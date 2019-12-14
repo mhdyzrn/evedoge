4b896d19f8fd739e683e4aca71e557ef6c49c29e
mhdyzrn <51892371+mhdyzrn@users.noreply.github.com>
2019-12-15 03:12:14 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,6 @@
+<?
+
+$UserAgent = "Mozilla/5.0 (Linux; Android 9; Mi A1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Mobile Safari/537.36";
+$Email = "mhdyuzron@gmail.com";
+$Password = "14061999";
+$PHPSESSID = "9q229cp2kqv6opkjc1168kcn73";
